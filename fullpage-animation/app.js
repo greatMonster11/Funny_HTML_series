e214152577a439a3ef658659531e82c621144331
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-17 17:05:03 +0700

    add animation for chairs movements

--- fullpage-animation/app.js
+++ fullpage-animation/app.js
@@ -8,5 +8,15 @@ new fullpage("#fullpage", {
     const title = section.querySelector("h1");
     const tl = new TimelineMax({ delay: 0.5 });
     tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: 0, opacity: 1 });
+
+    if (destination.index === 1) {
+      const chairs = document.querySelectorAll(".chair");
+      const description = document.querySelectorAll(".description");
+
+      tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-35%" })
+        .fromTo(chairs[0], 1, { opacity: 0 }, { opacity: 1 })
+        .fromTo(chairs[1], 1, { opacity: 0 }, { opacity: 1 })
+        .fromTo(chairs[2], 1, { opacity: 0 }, { opacity: 1 });
+    }
   }
 });
