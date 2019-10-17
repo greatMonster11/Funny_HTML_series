b9d0ad056d8cb771088f4390fc396e3c33a29dc1
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-17 16:57:30 +0700

    add animation for tilte of each section

--- fullpage-animation/app.js
+++ fullpage-animation/app.js
@@ -1,4 +1,12 @@
 new fullpage("#fullpage", {
+  licenseKey: "GPLv3",
   autoScrolling: true,
-  navigation: true
+  navigation: true,
+  onLeave: (origin, destination, direction) => {
+    const section = destination.item;
+    // console.log(section);
+    const title = section.querySelector("h1");
+    const tl = new TimelineMax({ delay: 0.5 });
+    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: 0, opacity: 1 });
+  }
 });
