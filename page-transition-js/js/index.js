b2c51699edaa107ffcdabf60aa60f3625fcb63cd
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-11-23 17:00:48 +0700

    add transition

--- page-transition-js/js/index.js
+++ page-transition-js/js/index.js
@@ -1,3 +1,8 @@
-const run = () => {
-  console.log("Yo, I'm running !!");
-};
+import Highway from "@dogstudio/highway";
+import Fade from "./transition";
+
+const H = new Highway.Core({
+  transitions: {
+    default: Fade
+  }
+});
