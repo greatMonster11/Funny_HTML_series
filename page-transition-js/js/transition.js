102f0f81ea4b7b53cc6a4c1338c13ae46c8acc76
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-11-23 21:04:18 +0700

    update animate transition

--- page-transition-js/js/transition.js
+++ page-transition-js/js/transition.js
@@ -4,14 +4,25 @@ import { TimelineLite } from "gsap";
 class Fade extends Highway.Transition {
   in({ from, to, done }) {
     const tl = TimelineLite();
-    tl.fromTo(to, 0.5, { left: "-100%", top: "50%" }, { left: "0%" }).fromTo(
-      to,
-      0.5,
-      { height: "2vh" },
-      { height: "90vh", top: "10%" }
-    );
+    tl.fromTo(to, 0.5, { left: "-100%", top: "50%" }, { left: "0%" })
+      .fromTo(
+        to,
+        0.5,
+        { height: "2vh" },
+        {
+          height: "90vh",
+          top: "10%",
+          onComplete: function() {
+            from.remove();
+            done();
+          }
+        }
+      )
+      .fromTo(to.children[0], 2, { opacity: 0 }, { opacity: 1 });
   }
-  out({ from, done }) {}
+  out({ from, done }) {
+    done();
+  }
 }
 
 export default Fade;
