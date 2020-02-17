080558526a50b3e3dea63086c763e2f2c6fe58f0
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-02-17 10:24:04 +0700

    feat(content): add animation with timing fuction and CDN

--- Facebook-reaction-buttons/src/index.js
+++ Facebook-reaction-buttons/src/index.js
@@ -1,1 +1,20 @@
 import "./main.scss";
+
+console.log("hey i'm here");
+
+$(function() {
+  $(".like-btn").hover(
+    function() {
+      $(".reaction-icon").each(function(i, e) {
+        setTimeout(function() {
+          $(e).addClass("show");
+        }, i * 100);
+      });
+    },
+    function() {
+      $(".reaction-icon").removeClass("show");
+    }
+  );
+});
+
+console.log("hello hteere");
