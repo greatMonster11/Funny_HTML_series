9d94159da9c7e6f89f5184c44a743d00c4edaf10
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-02-28 13:47:11 +0700

    feat(UI): get mouse location on screen

---
+++ dev.to-drawing/script.js
@@ -0,0 +1,30 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.setAttribute("width", window.innerWidth);
+canvas.setAttribute("height", window.innerHeight);
+
+let x, y, isPainting;
+
+function getCoordinates(event) {
+  // check to see if mobile or desktop
+  if (["mousedown", "mousemove"].includes(event.type)) {
+    // click events
+    return [event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop];
+  } else {
+    // touch coordinates
+    return [
+      event.touches[0].pageX - canvas.offsetLeft,
+      event.touches[0].pageY - canvas.offsetTop
+    ];
+  }
+}
+
+function startPaint(e) {
+  isPainting = true;
+  let coordinates = getCoordinates(e);
+  console.log(coordinates);
+}
+
+canvas.addEventListener("mousedown", startPaint);
+canvas.addEventListener("touchstart", startPaint);
